3c3e60df8c696a4fb28bcd8b964ccbe5cbe3b1ad
Yung Gie <lingyunggie@gmail.com>
2020-08-19 22:02:33 +0800

    fix view order crash after user deleted

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,7 +9,7 @@ class Order extends Model
 {
     public function user()
     {
-        return $this->belongsTo(User::class, 'user_id');
+        return $this->belongsTo(User::class, 'user_id')->withTrashed();
     }
 
     public function food()
